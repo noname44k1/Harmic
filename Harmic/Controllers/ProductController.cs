4606d14e6b781e1d143d3c989ed1f7502dd6633a
le thanh <thanh.cntt.dhv@gmail.com>
2024-04-15 09:02:48 +0700

    15/4/2024

---
+++ Harmic/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+
+namespace Harmic.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HarmicContext _context;
+
+        public ProductController(HarmicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/product/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbProducts == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.TbProducts.Include(i => i.CategoryProduct)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.productReview = _context.TbProductReviews.Where(i => i.ProductId == id && i.IsActive).ToList();
+            ViewBag.productRelated = _context.TbProducts.Where(i => i.ProductId != id && i.CategoryProductId == product.CategoryProductId).Take(5).OrderByDescending(i => i.ProductId).ToList();
+            return View(product);
+        }
+    }
+}
---
+++ Harmic/Utilities/Function.cs
@@ -0,0 +1,10 @@
+﻿namespace Harmic.Utilities
+{
+    public class Function
+    {
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
