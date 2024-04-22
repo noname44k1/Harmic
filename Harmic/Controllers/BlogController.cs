eec957141c9ee21722773295b4d1fc395064438b
le thanh <thanh.cntt.dhv@gmail.com>
2024-04-22 08:05:18 +0700

    22/4/2024

--- Harmic/Controllers/BlogController.cs
+++ Harmic/Controllers/BlogController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Harmic.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly HarmicContext _context;
+        public BlogController(HarmicContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("/blog/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbBlogs == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.TbBlogs
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            ViewBag.blogComment = _context.TbBlogComments.Where(i => i.BlogId == id).ToList();
+            return View(blog);
         }
     }
 }
--- Harmic/Controllers/ProductController.cs
+++ Harmic/Controllers/ProductController.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.IdentityModel.Tokens;
-
 namespace Harmic.Controllers
 {
     public class ProductController : Controller
@@ -34,8 +31,11 @@ namespace Harmic.Controllers
             {
                 return NotFound();
             }
-            ViewBag.productReview = _context.TbProductReviews.Where(i => i.ProductId == id && i.IsActive).ToList();
-            ViewBag.productRelated = _context.TbProducts.Where(i => i.ProductId != id && i.CategoryProductId == product.CategoryProductId).Take(5).OrderByDescending(i => i.ProductId).ToList();
+            ViewBag.productReview = _context.TbProductReviews.
+                Where(i => i.ProductId == id && i.IsActive).ToList();
+            ViewBag.productRelated = _context.TbProducts.
+                Where(i => i.ProductId != id && i.CategoryProductId == product.CategoryProductId).Take(5).
+                OrderByDescending(i => i.ProductId).ToList();
             return View(product);
         }
     }
